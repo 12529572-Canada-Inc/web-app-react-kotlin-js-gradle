1189411e32c276aeaa20d3c9ff7c30d95472aa1c
djedi-knight <shawn.daichendt@gmail.com>
2021-06-17 00:48:12 -0400

    Created/ implemented VideoListProps

--- src/main/kotlin/VideoList.kt
+++ src/main/kotlin/VideoList.kt
@@ -1,11 +1,16 @@
 import react.*
 import react.dom.*
 
+external interface VideoListProps: RProps {
+    var videos: List<Video>
+}
+
 @JsExport
-class VideoList: RComponent<RProps, RState>() {
+class VideoList: RComponent<VideoListProps, RState>() {
     override fun RBuilder.render() {
-        for (video in unwatchedVideos) {
+        for (video in props.videos) {
             p {
+                key = video.id.toString()
                 +"${video.speaker}: ${video.title}"
             }
         }
