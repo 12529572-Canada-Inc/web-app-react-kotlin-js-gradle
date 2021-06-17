c07f1bc4ef34c898fea31c37fe18226342577d39
djedi-knight <shawn.daichendt@gmail.com>
2021-06-17 15:23:45 -0400

    Created/ implemented onSelectVideo prop

--- src/main/kotlin/VideoList.kt
+++ src/main/kotlin/VideoList.kt
@@ -6,6 +6,7 @@ import react.dom.*
 external interface VideoListProps: RProps {
     var videos: List<Video>
     var selectedVideo: Video?
+    var onSelectVideo: (Video) -> Unit
 }
 
 // Define React component class
@@ -17,9 +18,9 @@ class VideoList: RComponent<VideoListProps, RState>() {
                 key = video.id.toString()
                 attrs {
                     onClickFunction = {
-//                        setState {
-//                            selectedVideo = video
-//                        }
+                        setState {
+                            props.onSelectVideo(video)
+                        }
                     }
                 }
                 if (video == props.selectedVideo) {
