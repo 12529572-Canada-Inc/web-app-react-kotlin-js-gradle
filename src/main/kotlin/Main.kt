0295385bb411ea69b3cb047822a5760db251dcc5
djedi-knight <shawn.daichendt@gmail.com>
2021-06-17 00:26:55 -0400

    Refactored code to App component

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,74 +1,9 @@
 import react.dom.*
 import kotlinx.browser.document
-import kotlinx.css.*
-import styled.css
-import styled.styledDiv
-
-external interface Video {
-    val id: Int
-    val title: String
-    val speaker: String
-    val videoUrl: String
-}
-
-data class KotlinVideo(
-    override val id: Int,
-    override val title: String,
-    override val speaker: String,
-    override val videoUrl: String
-) : Video
 
 fun main() {
-    // Create list of watched/ unwatched videos
-    val unwatchedVideos = listOf(
-        KotlinVideo(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
-        KotlinVideo(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
-        KotlinVideo(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E"),
-    )
-
-    val watchedVideos = listOf(
-        KotlinVideo(4, "Mouse-less development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
-    )
-
-    // render the components
+    // Render the components
     render(document.getElementById("root")) {
         child(App::class) {}
-//        h1 {
-//            +"KotlinConf Explorer"
-//        }
-//        div {
-//            h3 {
-//                +"Videos to watch"
-//            }
-//            for(video in unwatchedVideos) {
-//                p {
-//                    +"${video.speaker}: ${video.title}"
-//                }
-//            }
-//
-//            h3 {
-//                +"Videos watched"
-//            }
-//            for(video in watchedVideos) {
-//                p {
-//                    +"${video.speaker}: ${video.title}"
-//                }
-//            }
-//        }
-//        styledDiv {
-//            css {
-//                position = Position.absolute
-//                top = 10.px
-//                right = 10.px
-//            }
-//            h3 {
-//                +"John Doe: Building and breaking things"
-//            }
-//            img {
-//                attrs {
-//                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
-//                }
-//            }
-//        }
     }
 }
