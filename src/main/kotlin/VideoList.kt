e9bb9ecc565ae017ed61bc63a08e9a65c211d312
djedi-knight <shawn.daichendt@gmail.com>
2021-06-17 15:15:23 -0400

    Implemented VideoListState

--- src/main/kotlin/VideoList.kt
+++ src/main/kotlin/VideoList.kt
@@ -1,4 +1,3 @@
-import kotlinx.browser.window
 import kotlinx.html.js.onClickFunction
 import react.*
 import react.dom.*
@@ -15,15 +14,20 @@ external interface VideoListState: RState {
 
 // Define class
 @JsExport
-class VideoList: RComponent<VideoListProps, RState>() {
+class VideoList: RComponent<VideoListProps, VideoListState>() {
     override fun RBuilder.render() {
         for (video in props.videos) {
             p {
                 key = video.id.toString()
                 attrs {
                     onClickFunction = {
-                        window.alert("Clicked $video!")
+                        setState {
+                            selectedVideo = video
+                        }
                     }
+                }
+                if (video == state.selectedVideo) {
+                    +"▶ "
                 }
                 +"${video.speaker}: ${video.title}"
             }
