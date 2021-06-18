73f7cfad275eaf276d4adda887e413519692bdde
djedi-knight <shawn.daichendt@gmail.com>
2021-06-17 23:56:07 -0400

    Created Counter functional component example

---
+++ src/main/kotlin/Counter.kt
@@ -0,0 +1,25 @@
+import kotlinx.html.js.onClickFunction
+import react.*
+import react.dom.button
+import react.dom.h1
+
+external interface CounterProps: RProps {
+    var name: String
+}
+
+val counter = functionalComponent<CounterProps> { props ->
+    val (count, setCount) = useState(0)
+    h1 {
+        +props.name
+    }
+    button {
+        attrs.onClickFunction = { setCount(count + 1) }
+        +count.toString()
+    }
+}
+
+fun RBuilder.counter(handler: CounterProps.() -> Unit): ReactElement = child(counter) {
+    attrs {
+        handler()
+    }
+}
